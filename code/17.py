e7554fe0a3ed51b2345cf5569014983d8146a75e
Ian Layzer <ianlayzer@gmail.com>
2022-12-19 21:30:12 -0500

    Completed Day 17 Part 1

--- code/17.py
+++ code/17.py
@@ -13,158 +13,147 @@ def checkIfAir(chamber, r, c):
     return r >= 0 and r < len(chamber) and c >= 0 and c < len(chamber[0]) and chamber[r][c] == AIR
 
 class MinusRock:
-    def __init__(self, chamber):
-        self.chamber = chamber
+    def __init__(self):
         self.currRow, self.currCol = ROCK_START_POS
         self.height = 1
+
+    def getDownNeighbors(self):
+        return [(self.currRow + 1, c) for c in range(self.currCol, self.currCol + 4)]
+
+    def getLeftNeighbors(self):
+        return [(self.currRow, self.currCol - 1)]
+
+    def getRightNeighbors(self):
+        return [(self.currRow, self.currCol + 4)]
+
+    def getPositions(self):
+        return [(self.currRow, c) for c in range(self.currCol, self.currCol + 4)]
     
-    def goDown(self):
-        for c in range(self.currCol, self.currCol + 4):
-            if not checkIfAir(self.chamber, self.currRow + 1, c):
-                return False
-        self.currRow += 1
-        return True
-    
-    def goLeft(self):
-        if not checkIfAir(self.chamber, self.currRow, self.currCol - 1):
-            return False
-        self.currCol -= 1
-        return True
-
-    def goRight(self):
-        if not checkIfAir(self.chamber, self.currRow, self.currCol + 4):
-            return False
-        self.currCol += 1
-        return True
-
-    def fill(self, char):
-        for c in range(self.currCol, self.currCol + 4):
-            self.chamber[self.currRow][c] = char
-
 class PlusRock:
-    def __init__(self, chamber):
-        self.chamber = chamber
+    def __init__(self):
         self.currRow, self.currCol = ROCK_START_POS
         self.height = 3
 
-    def goDown(self):
-        if not checkIfAir(self.chamber, self.currRow + 3, self.currCol + 1):
-            return False
-        self.currRow += 1
-        return True
-    
-    def goLeft(self):
-        if not checkIfAir(self.chamber, self.currRow + 1, self.currCol - 1):
-            return False
-        self.currCol -= 1
-        return True
+    def getDownNeighbors(self):
+        return [
+            (self.currRow + 2, self.currCol),
+            (self.currRow + 3, self.currCol + 1),
+            (self.currRow + 2, self.currCol + 2)
+        ]
 
-    def goRight(self):
-        if not checkIfAir(self.chamber, self.currRow + 1, self.currCol + 1):
-            return False
-        self.currCol += 1
-        return True
+    def getLeftNeighbors(self):
+        return [
+            (self.currRow, self.currCol),
+            (self.currRow + 1, self.currCol - 1),
+            (self.currRow + 2, self.currCol)
+        ]
 
-    def fill(self, char):
-        self.chamber[self.currRow][self.currCol + 1] = char
-        self.chamber[self.currRow + 1][self.currCol] = char
-        self.chamber[self.currRow + 1][self.currCol + 1] = char
-        self.chamber[self.currRow + 1][self.currCol + 2] = char
-        self.chamber[self.currRow + 2][self.currCol + 1] = char
+    def getRightNeighbors(self):
+        return [
+            (self.currRow, self.currCol + 2),
+            (self.currRow + 1, self.currCol + 3),
+            (self.currRow + 2, self.currCol + 2)
+        ]
+
+    def getPositions(self):
+        return [
+            (self.currRow, self.currCol + 1),
+            (self.currRow + 1, self.currCol),
+            (self.currRow + 1, self.currCol + 1),
+            (self.currRow + 1, self.currCol + 2),
+            (self.currRow + 2, self.currCol + 1),
+        ]
 
 class LRock:
-    def __init__(self, chamber):
-        self.chamber = chamber
+    def __init__(self):
         self.currRow, self.currCol = ROCK_START_POS
         self.height = 3
 
-    def goDown(self):
-        for c in range(self.currCol, self.currCol + 3):
-            if not checkIfAir(self.chamber, self.currRow + 3, c):
-                return False
-        self.currRow += 1
-        return True
-    
-    def goLeft(self):
-        if not checkIfAir(self.chamber, self.currRow + 2, self.currCol - 1):
-            return False
-        self.currCol -= 1
-        return True
+    def getDownNeighbors(self):
+        return [(self.currRow + 3, c) for c in range(self.currCol, self.currCol + 3)]
 
-    def goRight(self):
-        for r in range(self.currRow, self.currRow + 3):
-            if not checkIfAir(self.chamber, r, self.currCol + 1):
-                return False
-        self.currCol += 1
-        return True
+    def getLeftNeighbors(self):
+        return [
+            (self.currRow, self.currCol + 1),
+            (self.currRow + 1, self.currCol + 1),
+            (self.currRow + 2, self.currCol - 1),
+        ]
 
-    def fill(self, char):
-        for c in range(self.currCol, self.currCol + 3):
-            self.chamber[self.currRow + 2][c] = char
-        for r in range(self.currRow, self.currRow + 3):
-            self.chamber[r][self.currCol + 2] = char
+    def getRightNeighbors(self):
+        return [(r, self.currCol + 3) for r in range(self.currRow, self.currRow + 3)]
 
+
+    def getPositions(self):
+        return [
+            (self.currRow, self.currCol + 2),
+            (self.currRow + 1, self.currCol + 2),
+            (self.currRow + 2, self.currCol),
+            (self.currRow + 2, self.currCol + 1),
+            (self.currRow + 2, self.currCol + 2),
+        ]
 class ThinRock:
-    def __init__(self, chamber):
-        self.chamber = chamber
+    def __init__(self):
         self.currRow, self.currCol = ROCK_START_POS
         self.height = 4
-    
-    def goDown(self):
-        if not checkIfAir(self.chamber, self.currRow + 4, self.currCol):
-            return False
-        self.currRow += 1
-        return True
-    
-    def goLeft(self):
-        for r in range(self.currRow, self.currRow + 4):
-            if not checkIfAir(self.chamber, r, self.currCol - 1):
-                return False
-        self.currCol -= 1
-        return True
 
-    def goRight(self):
-        for r in range(self.currRow, self.currRow + 4):
-            if not checkIfAir(self.chamber, r, self.currCol + 1):
-                return False
-        self.currCol += 1
-        return True
+    def getDownNeighbors(self):
+        return [(self.currRow + 4, self.currCol)]
 
-    def fill(self, char):
-        for r in range(self.currRow, self.currRow + 4):
-            self.chamber[r][self.currCol] = char
+    def getLeftNeighbors(self):
+        return [(r, self.currCol - 1) for r in range(self.currRow, self.currRow + 4)]
+
+    def getRightNeighbors(self):
+        return [(r, self.currCol + 1) for r in range(self.currRow, self.currRow + 4)]
+
+    def getPositions(self):
+        return [(r, self.currCol) for r in range(self.currRow, self.currRow + 4)]
 
 class SquareRock:
-    def __init__(self, chamber):
-        self.chamber = chamber
+    def __init__(self):
         self.currRow, self.currCol = ROCK_START_POS
         self.height = 2
     
-    def goDown(self):
-        for c in range(self.currCol, self.currCol + 2):
-            if not checkIfAir(self.chamber, self.currRow + 2, c):
-                return False
-        self.currRow += 1
-        return True
-    
-    def goLeft(self):
-        for r in range(self.currRow, self.currRow + 2):
-            if not checkIfAir(self.chamber, r, self.currCol - 1):
-                return False
-        self.currRow -= 1
-        return True
+    def getDownNeighbors(self):
+        return [(self.currRow + 2, c) for c in range(self.currCol, self.currCol + 2)]
 
-    def goRight(self):
-        for r in range(self.currRow, self.currRow + 2):
-            if not checkIfAir(self.chamber, r, self.currCol + 1):
-                return False
-        self.currRow += 1
-        return True
+    def getLeftNeighbors(self):
+        return [(r, self.currCol - 1) for r in range(self.currRow, self.currRow + 2)]
 
-    def fill(self, char):
+    def getRightNeighbors(self):
+        return [(r, self.currCol + 2) for r in range(self.currRow, self.currRow + 2)]
+
+    def getPositions(self):
+        positions = []
         for r in range(self.currRow, self.currRow + 2):
             for c in range(self.currCol, self.currCol + 2):
-                self.chamber[r][c] = char
+                positions.append((r, c))
+        return positions
+
+def goDown(chamber, rock):
+    for r, c in rock.getDownNeighbors():
+        if not checkIfAir(chamber, r, c):
+            return False
+    rock.currRow += 1
+    return True
+
+def goLeft(chamber, rock):
+    for r, c in rock.getLeftNeighbors():
+        if not checkIfAir(chamber, r, c):
+            return False
+    rock.currCol -= 1
+    return True
+
+def goRight(chamber, rock):
+    for r, c in rock.getRightNeighbors():
+        if not checkIfAir(chamber, r, c):
+            return False
+    rock.currCol += 1
+    return True
+
+def fill(chamber, rock, char):
+    for r, c in rock.getPositions():
+        chamber[r][c] = char    
+
 
 ROCK_CLASSES = [MinusRock, PlusRock, LRock, ThinRock, SquareRock]
 
@@ -178,38 +167,56 @@ def printChamber(chamber):
 def simulateRocks(jets, numRocks):
     chamber = [[AIR] * CHAMBER_WIDTH for _ in range(4)] + [[FLOOR] * CHAMBER_WIDTH]
     def printCurr(rk):
-        rk.fill("@")
+        fill(chamber, rk, "@")
         printChamber(chamber)
-        rk.fill(".")
+        fill(chamber, rk, ".")
 
     topRock = float('inf')
+    jetIndex = 0
     for r in range(numRocks):
-        rock = ROCK_CLASSES[r % 5](chamber)
-        heightIncreaseNeeded = rock.height + 4 - topRock
-        
-        printCurr(rock)
-        jetIndex = 0
+        # print("A new rock begins falling:")
+        rock = ROCK_CLASSES[r % 5]()
+        heightIncrease = rock.height + 3 - topRock
+        if heightIncrease > 0:
+            addedAir = [[AIR] * CHAMBER_WIDTH for _ in range(heightIncrease)]
+            chamber = addedAir + chamber
+            topRock += heightIncrease
+        elif heightIncrease != float('-inf'):
+            rock.currRow = -heightIncrease
+
+        # printCurr(rock)
         isFalling = True
         while isFalling:
-            jet = jets[jetIndex]
-            if jet == LEFT:
-                rock.goLeft()
-            elif jet == RIGHT:
-                rock.goRight()
-            printCurr(rock) 
-            isFalling = rock.goDown()
-            printCurr(rock)
+            jet = jets[jetIndex % len(jets)]
             jetIndex += 1
-        topRock = min(topRock, rock.currRow)
-        print(topRock)
-        rock.fill(ROCK)
-        printChamber(chamber)
+            if jet == LEFT:
+                goLeft(chamber, rock)
+                # if goLeft(chamber, rock):
+                #     print("Jet of gas pushes rock left:")
+                # else:
+                #     print("Jet of gas pushes rock left, but nothing happens:")
+            elif jet == RIGHT:
+                goRight(chamber, rock)
+                # if goRight(chamber, rock):
+                #     print("Jet of gas pushes rock right:")
+                # else:
+                #     print("jet of gas pushes rock right, but nothing happens:")
 
+            # printCurr(rock) 
+            isFalling = goDown(chamber, rock)
+            # if isFalling:
+            #     print("Rock falls 1 unit:")
+            #     printCurr(rock)
+        topRock = min(topRock, rock.currRow)
+        fill(chamber, rock, ROCK)
+        # print("Rock falls 1 unit, causing it to come to rest:")
+        # printChamber(chamber)
+    print("End Result:")
     printChamber(chamber)
-    return 0
+    return len(chamber) - topRock - 1
 
 def solve(file):
     jets = parse(file)
-    print(f"Part 1: {simulateRocks(jets, 2)}")
+    print(f"Part 1: {simulateRocks(jets, 2022)}")
 
-solve("inputs/17/small.txt")
+solve("inputs/17/full.txt")
